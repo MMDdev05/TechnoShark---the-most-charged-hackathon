4e7213e4e00c01cc5ebaef19ab20653f2aab6635
stasya11196 <rogovivan120706@gmail.com>
2024-11-22 21:17:29 +0300

    Repaired version

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 from typing import Optional
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Column
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
@@ -12,16 +12,10 @@ class Base(DeclarativeBase):
 
 class B_size(Base):
     __tablename__ = "size_table"
-    b_size: Mapped[str] = mapped_column(primary_key=True)
-    help: Mapped["Common_table"] = mapped_column(ForeignKey("common.help"))
-
-    def __repr__(self) -> str:
-        return f"B_size(b_size={self.b_size!r}, help={self.help!r})"
+    size = Column(String, primary_key=True)
+    help = relationship("Common_table", backref="help")
 
 class Common_table(Base):
     __tablename__ = "common"
-    help: Mapped[str] = mapped_column(primary_key=True)
-    b_size: Mapped["B_size"] = mapped_column(ForeignKey("size_table.b_size"))
-
-    def __repr__(self) -> str:
-        return f"Common(help={self.help!r}, b_size={self.b_size!r})"
+    help = Column(String, primary_key=True)
+    size = relationship("B_size", backref='size')
